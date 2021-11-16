d4f93bdd280e4c8b475b182c4c6e56f76a2abfe4
TekNiBaLL <jexiao@bsu.edu>
2021-11-16 16:40:08 -0500

    featureA test complete

--- features/stepdefinitions/stepdefs.js
+++ features/stepdefinitions/stepdefs.js
@@ -2,3 +2,24 @@ const assert = require("assert");
 const { Given, When, Then } = require("@cucumber/cucumber");
 const exp = require("constants");
 
+// Feature 1 tests
+Given('I am a leader', function(){
+  this.userType = 'leader';
+});
+
+When('I {}move through lyrics', function(expectedAnswer){
+  if(this.userType == "leader"){
+    if(expectedAnswer === ""){
+      this.actualAnswer = "can";
+    }
+  }else{
+    this.actualAnswer = "can't";
+  }
+});
+
+Then('I {} sing while playing', function(expectedAnswer){
+  assert.strictEqual(this.actualAnswer, expectedAnswer);
+});
+
+
+// Feature 2
